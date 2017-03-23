8107c3dac55a502c0c3cf28c2988ef46d52801dc
myrionsc <marpht23@gmail.com>
2017-03-23 19:27:23 +0000

    socket io almost working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  private socket = io();
+
+  
+
+
+
+
+
+// test data
+//   dataarr = [
+//   {
+//     "activity": "længdespring",
+//     "team": "hold1",
+//     "time": 90
+//   },
+//   {
+//     "activity": "vikingespil",
+//     "team": "hold2",
+//     "time": 32
+//   },
+//   {
+//     "activity": "svømning",
+//     "team": "hold3",
+//     "time": 42
+//   },
+//   {
+//     "activity": "blah",
+//     "team": "hold4",
+//     "time": 80.234
+//   }
+// ];
+
+
+
+  // socket.on('score_change', function(msg){
+  //   // $('#messages').append($('<li>').text(msg));
+  //   console.log(msg);
+  // });
+
+  // data = [1,2,3,4,5,6,7,];
 }
+
+
