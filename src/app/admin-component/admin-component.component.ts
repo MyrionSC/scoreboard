3b2bcf481201caab3ab1073021cc0ae9fd483a8f
myrionsc <marpht23@gmail.com>
2017-03-28 20:04:51 +0000

    admin panel almost done

--- src/app/admin-component/admin-component.component.ts
+++ src/app/admin-component/admin-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SocketService } from '../socket.service';
 
 @Component({
@@ -7,9 +7,31 @@ import { SocketService } from '../socket.service';
   styleUrls: ['./admin-component.component.css'],
   providers: [SocketService]
 })
-export class AdminComponentComponent implements OnInit {
+export class AdminComponentComponent implements OnInit, OnDestroy {
+  private score: Array<any> = [
+    {
+      "activity": "længdespring",
+      "team": "hold1",
+      "time": 90
+    },
+    {
+      "activity": "vikingespil",
+      "team": "hold2",
+      "time": 32
+    },
+    {
+      "activity": "svømning",
+      "team": "hold3",
+      "time": 42
+    },
+    {
+      "activity": "blah",
+      "team": "hold4",
+      "time": 80.234
+    }
+  ];
 
-  private testdata: Array<any> = [
+  private oldScore: Array<any> = [
     {
       "activity": "længdespring",
       "team": "hold1",
@@ -35,10 +57,24 @@ export class AdminComponentComponent implements OnInit {
   constructor(private socketService: SocketService) { }
 
   ngOnInit() {
-
+    // this.socketService.on("score_change", (new_score) => {
+    //   console.log("score change received from server:\n");
+    //   console.log(new_score);
+    //   // somehow angular doesn't understand that json loaded from a file is an array, so eval() has to be used then
+    //   this.score = new_score instanceof Array ? new_score : eval(new_score);
+    //   }
+    // )
   }
 
-  changeScore():void {
-    this.socketService.emit("new_score", this.testdata)
+  ngOnDestroy () {
+    this.socketService.off("score_change");
+  }
+
+  saveScore():void {
+    console.log(this.score);
+    // this.socketService.emit("new_score", this.testdata)
+  }
+  revertChanges():void {
+    this.score = this.oldScore;
   }
 }
