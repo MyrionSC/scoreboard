500ae3046fd843c1716b2aa39438cae696bbaf84
myrionsc <marpht23@gmail.com>
2017-03-29 14:20:36 +0000

    some refactor

--- src/app/admin-component/admin-component.component.ts
+++ src/app/admin-component/admin-component.component.ts
@@ -11,51 +11,6 @@ import {Activity} from "../activity";
 export class AdminComponentComponent implements OnInit, OnDestroy {
   private score: Array<Activity>;
   private oldScore: Array<Activity>;
-  // private score: Array<Activity> = [
-  //   {
-  //     "activity": "længdespring",
-  //     "team": "hold1",
-  //     "time": 90
-  //   },
-  //   {
-  //     "activity": "vikingespil",
-  //     "team": "hold2",
-  //     "time": 32
-  //   },
-  //   {
-  //     "activity": "svømning",
-  //     "team": "hold3",
-  //     "time": 42
-  //   },
-  //   {
-  //     "activity": "blah",
-  //     "team": "hold4",
-  //     "time": 80.234
-  //   }
-  // ];
-  //
-  // private oldScore: Array<Activity> = [
-  //   {
-  //     "activity": "længdespring",
-  //     "team": "hold1",
-  //     "time": 90
-  //   },
-  //   {
-  //     "activity": "vikingespil",
-  //     "team": "hold2",
-  //     "time": 32
-  //   },
-  //   {
-  //     "activity": "svømning",
-  //     "team": "hold3",
-  //     "time": 42
-  //   },
-  //   {
-  //     "activity": "blah",
-  //     "team": "hold4",
-  //     "time": 80.234
-  //   }
-  // ];
 
   constructor(private socketService: SocketService) { }
 
@@ -63,8 +18,6 @@ export class AdminComponentComponent implements OnInit, OnDestroy {
     this.socketService.on("score_change", (new_score) => {
       console.log("score change received from server:\n");
       console.log(new_score);
-      // somehow angular doesn't understand that json loaded from a file is an array, so eval() has to be used then
-      // this.score = new_score instanceof Array ? new_score : eval(new_score);
       this.score = this.copyArray(new_score);
       this.oldScore = this.copyArray(new_score);
       }
@@ -94,11 +47,11 @@ export class AdminComponentComponent implements OnInit, OnDestroy {
   }
   revertChanges():void {
     console.log("reverting changes");
-    // roundabout way to get copy by value instead of copy by reference
     this.score = this.copyArray(this.oldScore);
   }
 
   private copyArray (ar: Array<Activity>): Array<Activity> {
+    // roundabout way to get copy by value instead of copy by reference
     return JSON.parse(JSON.stringify(ar));
   }
 }
--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -10,14 +10,12 @@ import { SocketService } from '../socket.service';
 export class ScoreboardComponent implements OnInit, OnDestroy {
   score: Array<any>;
 
-  constructor(private socketService: SocketService) {
-  }
+  constructor(private socketService: SocketService) {}
 
   ngOnInit() {
     this.socketService.on('score_change', (new_score) => {
       console.log("score change received from server:\n");
       console.log(new_score);
-      // somehow angular doesn't understand that json loaded from a file is an array, so eval() has to be used then
       this.score = new_score;
     });
   }
